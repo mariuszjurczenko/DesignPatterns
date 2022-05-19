a12973670690d35658c877f73cb0972ee4b22377
mariusz <mariuszjurczenko@gmail.com>
2022-05-19 09:10:22 +0200

    Singleton

---
+++ Singleton/Program.cs
@@ -0,0 +1,13 @@
+﻿using Singleton;
+
+var instance1 = Logger.Instance;
+var instance2 = Logger.Instance;
+
+if (instance1 == instance2 && instance2 == Logger.Instance)
+{
+    Console.WriteLine("Instancje są takie same.");
+}
+
+instance1.Log($"Wiadomość od {nameof(instance1)}");
+instance1.Log($"Wiadomość od {nameof(instance2)}");
+Logger.Instance.Log($"Wiadomość od {nameof(Logger.Instance)}");
